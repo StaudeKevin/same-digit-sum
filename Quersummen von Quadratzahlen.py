2d5c410c1aed5ce11069da046677d51e13329805
Kev <social.92@live.de>
2021-11-25 17:42:07 +0100

    Add files via upload

---
+++ Quersummen von Quadratzahlen.py
@@ -0,0 +1,22 @@
+# find numbers n such that n and n^2 have same digit sum
+
+def sum_digit(i):
+	"""Die Funktion berechnet die Quersumme von i."""
+	summe = 0
+	for digit in str(i):
+		summe += int(digit)
+	return summe
+
+def check_same_digit(m):
+	"""Die Funktion checkt, welche Zahlen von 0 bis m die gleiche Quersumme wie ihre Quadratzahlen haben."""
+	i = 0
+	while i <= m:
+		if sum_digit(i) == sum_digit(i**2):
+			numbers.append(i)
+		else:
+			pass
+		i += 1
+
+numbers = []
+check_same_digit(1000)
+print(numbers)
